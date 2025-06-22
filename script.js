1a91f69f73b38314c30f65492fe09237989dc021
ayushv <rwtsiya025@gmail.com>
2025-06-22 14:32:32 +0530

    Added Dialog and its Functionality in Js

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.querySelector("#form");
+const openButton = document.querySelector("[data-open-modal]");
+const closeButton = document.querySelector("[data-close-modal]");
+const modal = document.querySelector("[data-modal]");
+
+openButton.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeButton.addEventListener("click",() => { 
+    modal.close();
+ })
