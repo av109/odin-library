bc6ae308d4455e34fba19f0faf649860afe00580
ayushv <rwtsiya025@gmail.com>
2025-06-22 14:41:36 +0530

    Constructor, and addBooksToLibrary functionality

--- script.js
+++ script.js
@@ -3,10 +3,38 @@ const openButton = document.querySelector("[data-open-modal]");
 const closeButton = document.querySelector("[data-close-modal]");
 const modal = document.querySelector("[data-modal]");
 
+let myLibrary = [];
+
+class Book {
+  constructor(name, author, pages, isRead) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+  }
+
+  toggleRead() {
+    this.isRead = !this.isRead;
+  }
+}
+
+function addBookToLibrary(name, author, pages, isRead) {
+  const book = new Book(name, author, pages, isRead);
+  myLibrary.push(book);
+}
+
+function displayBooks(){
+
+}
+
 openButton.addEventListener("click", () => {
   modal.showModal();
 });
 
-closeButton.addEventListener("click",() => { 
-    modal.close();
- })
+closeButton.addEventListener("click", () => {
+  modal.close();
+});
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("1984", "George Orwell", 328, true);
+addBookToLibrary("Dune", "Frank Herbert", 412, false);
